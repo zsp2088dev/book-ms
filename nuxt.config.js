e9ed178c7d4e4c2a13d371f47a809a09f4a5d010
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-02 10:27:14 +0900

    Init Project!

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+  mode: 'spa',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Book MS',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '図書管理システムへようこそ。ログインして研究室にある書籍を確認してみましょう。'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: ['element-ui/lib/theme-chalk/index.css'],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/element-ui'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <p>Init</p>
+  </div>
+</template>
