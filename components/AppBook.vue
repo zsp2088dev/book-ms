3602173b9b486bc6e699903d6ad300dbd8a63b4f
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-03 12:58:05 +0900

    [Add] Book

---
+++ components/AppBook.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app-book">
+    <el-card>
+      <img :src="path" alt="icon" class="image" width="100" height="100" />
+      <p>{{ title }}</p>
+      <p>{{ author }}</p>
+      <p>{{ price }}円</p>
+
+      <div style="text-align: left">
+        <el-tag
+          v-for="(tag, index) in tags"
+          :key="index"
+          effect="dark"
+          class="tag"
+        >
+          {{ tag }}
+        </el-tag>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBook',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    author: {
+      type: String,
+      default: ''
+    },
+    price: {
+      type: Number,
+      default: 0
+    },
+    tags: {
+      type: Array,
+      default: null
+    }
+  },
+  computed: {
+    path() {
+      return require(`@/assets/images/note.svg`)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-book {
+  text-align: center;
+  width: 250px;
+}
+
+.tag {
+  margin: 0.25rem;
+}
+</style>
