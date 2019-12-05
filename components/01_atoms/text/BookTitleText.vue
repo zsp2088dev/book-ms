6080b5b30cded9f2371f1e3a45907e3f555de4b5
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-05 17:03:51 +0900

    [Add] Book Title component

---
+++ components/01_atoms/text/BookTitleText.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="book-title-text">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookTitleText',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.book-title-text {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
