5c75823a2f724dad9cd99b2bf0a6e8c56b049d43
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-05 21:55:41 +0900

    [Add] Register Book Button component

---
+++ components/01_atoms/button/RegisterBookButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="register-book-button">
+    <el-button icon="el-icon-plus" class="register-book-el-button">
+      新規登録
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterBookButton'
+}
+</script>
+
+<style scoped>
+.register-book-el-button {
+  height: 50px;
+}
+</style>
