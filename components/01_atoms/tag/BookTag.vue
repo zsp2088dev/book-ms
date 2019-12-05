52a9c6e993508ceb72cc3e5830929aebb81ae26b
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-05 17:17:28 +0900

    [Add] Book Tag component

---
+++ components/01_atoms/tag/BookTag.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="book-tag">
+    <el-tag effect="plain"> #{{ tag }} </el-tag>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookTag',
+  props: {
+    tag: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
