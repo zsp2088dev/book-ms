b542ec53c2c7913ef4015784d30704c841e2acd2
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-05 17:50:28 +0900

    [Add] Book Card List component

---
+++ components/03_organisms/scoped/BookCardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="book-card-list">
+    <el-row :gutter="20">
+      <el-col
+        v-for="(book, index) in books"
+        :key="index"
+        :span="6"
+        :offset="0"
+        class="book-card-list-col"
+      >
+        <book-card
+          :title="book.title"
+          :author="book.author"
+          :price="book.price"
+          :tags="book.tags"
+        />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import BookCard from '../../02_molecules/card/BookCard'
+export default {
+  name: 'BookCardList',
+  components: { BookCard },
+  props: {
+    books: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped></style>
