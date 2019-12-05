fc3681e17629c42c291879518b32b236768dff80
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-05 21:51:12 +0900

    [Add] App Logo components

---
+++ components/01_atoms/icon/AppLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-logo">
+    <img :src="src" alt="app-logo" width="224" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLogo',
+  computed: {
+    src() {
+      return require(`@/assets/images/logo.svg`)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
