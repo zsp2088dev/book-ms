3a2f016bf6841af77fc8bd8e46ee5afac52f95fd
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-06 01:20:41 +0900

    [Add] Auth Button

---
+++ components/01_atoms/button/SignOutButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sign-out-button">
+    <el-button @click="signInGoogle" plain>SignOut</el-button>
+  </div>
+</template>
+
+<script>
+import { signOut } from '~/plugins/firebase'
+
+export default {
+  name: 'SignOutButton',
+  methods: {
+    signInGoogle() {
+      signOut()
+    }
+  }
+}
+</script>
+
+<style scoped></style>
