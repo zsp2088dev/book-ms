00a48eaaeb8945afbe956e9827495c602d75a4db
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-14 10:36:12 +0900

    [Add] Publication Date to Card

---
+++ components/01_atoms/text/BookPublicationDateText.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="book-publication-date-text">
+    <p>{{ toDateString }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookPublicationDateText',
+  props: {
+    date: {
+      type: Object,
+      default: () => ({
+        seconds: 0
+      })
+    }
+  },
+  computed: {
+    toDateString() {
+      const timestamp = new Date(this.date.seconds * 1000)
+      const year = timestamp.getFullYear()
+      const month = timestamp.getMonth() + 1
+      const date = timestamp.getDate()
+      return `${year}年${month}月${date}日`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.book-publication-date-text {
+  color: #606266;
+  font-size: 0.85rem;
+}
+</style>
