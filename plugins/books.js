3adceafb140acf5335e56f4279db3b0510eba1b4
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-16 03:06:51 +0900

    [Add] getBookFromGoogle function

--- plugins/books.js
+++ plugins/books.js
@@ -1,4 +1,5 @@
 import Fuse from 'fuse.js'
+import axios from 'axios'
 
 export const getFilteredBooks = (keyword, books) => {
   if (keyword.length === 0) {
@@ -10,4 +11,22 @@ export const getFilteredBooks = (keyword, books) => {
     keys: ['title', 'author', 'tags']
   }
   return new Fuse(books, options).search(keyword)
+}
+
+export const getBookFromGoogle = (isbn) => {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
+  return axios.get(url).then((r) => {
+    if (r.data.totalItems !== 1) {
+      return new Error('Unexpected ISBN')
+    }
+
+    const book = r.data.items[0].volumeInfo
+
+    return {
+      title: book.title,
+      subtitle: book.subtitle,
+      author: book.authors[0],
+      date: book.publishedDate
+    }
+  })
 }
