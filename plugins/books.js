c66801ec69af06444f325959ee9d33b52676407e
Kazuki Yamagata <zsp2088dev@gmail.com>
2019-12-16 17:29:36 +0900

    [Add] OpenBD API

--- plugins/books.js
+++ plugins/books.js
@@ -31,6 +31,41 @@ export const getBookFromGoogle = (isbn) => {
   })
 }
 
+export const getBookFromOpenBD = (isbn) => {
+  const url = `https://api.openbd.jp/v1/get?isbn=${isbn}`
+  return axios.get(url).then((r) => {
+    if (!r.data.length) {
+      return new Error('Unexpected ISBN')
+    }
+
+    const book = r.data[0].summary
+
+    // 取得した日付に関する操作
+    const pubDate = book.pubdate
+    let publicationDate
+    if (book.pubdate.match(/-/)) {
+      const includeDate = pubDate.split('-').length - 1 === 2
+      if (includeDate) {
+        publicationDate = pubDate.replace(/(\d+)-(\d+)-(\d+)/g, '$1年$2月$3日')
+      } else {
+        publicationDate = pubDate.replace(/(\d+)-(\d+)/g, '$1年$2月')
+      }
+    } else {
+      const year = pubDate.slice(0, 4) + '年'
+      const month = pubDate.slice(5, 7) + '月'
+      const date = pubDate.slice(8, 10) ? pubDate.slice(8, 10) + '日' : ''
+      publicationDate = year + month + date
+    }
+
+    return {
+      id: book.isbn,
+      title: book.title,
+      author: book.author.split('／')[0],
+      date: publicationDate
+    }
+  })
+}
+
 export const buildCheckedBooks = (checkedBooks, book) => {
   const setCheckedBooks = new Set(checkedBooks)
 
